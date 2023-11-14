9ea5afb63c2a2c0a9520c4748dfe9bd15230a68a
dh7hong <dh7hong@gmail.com>
2023-11-14 16:48:58 +0900

    Login, Signup, Authorization check, Logout

--- src/api/authService.js
+++ src/api/authService.js
@@ -6,7 +6,9 @@ const catchErrors = (error) => {
   if (error.response) {
     console.log("Error response status:", error.response.status);
     console.log("Error response data:", error.response.data.message);
-    alert(`Error response status: ${error.response.status}\n Error response data: ${error.response.data.message}`)
+    alert(
+      `Error response status: ${error.response.status}\n Error response data: ${error.response.data.message}`
+    );
   } else if (error.request) {
     console.log("No response received:", error.request);
   } else {
@@ -16,7 +18,10 @@ const catchErrors = (error) => {
 
 const registerUser = async (userData) => {
   try {
-    const response = await axios.post(`${process.env.REACT_APP_SERVER_URL}/register`, userData);
+    const response = await axios.post(
+      `${process.env.REACT_APP_SERVER_URL}/register`,
+      userData
+    );
     console.log(response);
     return response;
   } catch (error) {
@@ -26,7 +31,10 @@ const registerUser = async (userData) => {
 
 const loginUser = async (userData) => {
   try {
-    const response = await axios.post(`${process.env.REACT_APP_SERVER_URL}/login`, userData);
+    const response = await axios.post(
+      `${process.env.REACT_APP_SERVER_URL}/login`,
+      userData
+    );
 
     const token = response.data.token;
     const userId = JSON.parse(response.config.data).id;
@@ -46,11 +54,14 @@ const authUser = async () => {
   const authToken = store.getState().user.token;
 
   try {
-    const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/user`, {
-      headers: {
-        Authorization: `Bearer ${authToken}`,
-      },
-    });
+    const response = await axios.get(
+      `${process.env.REACT_APP_SERVER_URL}/user`,
+      {
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+      }
+    );
 
     if (response.status === 200) {
       console.log(response.data.message);
