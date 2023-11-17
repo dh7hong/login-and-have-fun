3b4cada4de222c6b415fcb2432a12111ff39f9eb
dh7hong <dh7hong@gmail.com>
2023-11-17 14:51:23 +0900

    prettier autosave

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,22 @@
-import React, { useState, useEffect } from "react";
-import { useMutation } from "react-query";
-import { useDispatch } from "react-redux";
-import { logout, setToken, setUserId } from "./redux/modules/userSlice";
-import { registerUser, loginUser, authUser } from "./api/authService";
-import axios from "axios";
-import store from "./redux/config/configStore";
-import Header from "./redux/components/Header/Header";
-import {
-  BrowserRouter,
-  Route,
-  Routes,
-  useNavigate,
-  Navigate,
-} from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import { QueryClient, QueryClientProvider } from "react-query";
-import PrivateRoute from "./shared/PrivateRoute";
-import Detail from "./pages/Detail";
-import Footer from "./redux/components/Footer/Footer";
+import React, { useState, useEffect } from 'react';
+import { useMutation } from 'react-query';
+import { useDispatch } from 'react-redux';
+import { logout, setToken, setUserId } from './redux/modules/userSlice';
+import { registerUser, loginUser, authUser } from './api/authService';
+import axios from 'axios';
+import store from './redux/config/configStore';
+import Header from './redux/components/Header/Header';
+import { BrowserRouter, Route, Routes, useNavigate, Navigate } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import PrivateRoute from './shared/PrivateRoute';
+import Detail from './pages/Detail';
+import Footer from './redux/components/Footer/Footer';
 
 function App() {
-  const isLoggedIn = !!localStorage.getItem("token"); // Check login status
+  const isLoggedIn = !!localStorage.getItem('token'); // Check login status
   const queryClient = new QueryClient();
 
   return (
@@ -40,10 +34,7 @@ function App() {
               </PrivateRoute>
             }
           />
-          <Route
-            path="/"
-            element={isLoggedIn ? <Navigate to="/home" /> : <Login />}
-          />
+          <Route path="/" element={isLoggedIn ? <Navigate to="/home" /> : <Login />} />
           <Route path="/:id" element={<Detail />} />
         </Routes>
         <Footer />
